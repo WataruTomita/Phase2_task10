38282e4f7c4931d0395d121b9d416f495ae6b437
WataruTomita <tomi.symphonia@gmail.com>
2021-02-08 23:02:41 +0900

    relationships

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
+  # Room relationship
   has_many :chats
   has_many :user_rooms
 end
--- app/models/user_room.rb
+++ app/models/user_room.rb
@@ -1,4 +1,5 @@
 class UserRoom < ApplicationRecord
+  # User room relationship
   belongs_to :user
   belongs_to :room
 end
